20c6415d80b9cb3fb9aec1df38ff580cf6b4a083
沖山敦樹 <okiyamaatsuki@okiyamaatsukinoMacBook-Air.local>
2025-07-18 14:27:31 +0900

    計算機課題

---
+++ math-index.js
@@ -0,0 +1,60 @@
+
+let sbNum = 0
+
+$("#answer").text(sbNum);
+
+
+$(".input").on("click",function(){
+  let num = $(this).text();
+  let numZ = $("#answer").text();
+
+  if (numZ === "0"){
+  $("#answer").text(num);
+  }else{
+  $("#answer").append(num);
+  }
+});
+
+
+$(".input_zero").on("click",function(){
+  let element_Num = $(this).text();
+  let element_Numa =$("#answer").text();
+
+  if (element_Numa === "0"){
+  return;
+  }
+
+  $("#answer").text(element_Num);
+  $("#answer").append(element_Num);       
+});
+
+
+$(".input_op").on("click",function(){
+  let sumNum = $(this).text();
+  let currentText = $("#answer").text();
+  let lastChar = currentText.slice(-1);
+
+  if (["+","-","*","/","."].includes(lastChar)){
+  return;
+  }
+
+  $("#answer").append(sumNum);
+});
+
+
+$(".input_ans").on("click",function(){
+  let exp = $("#answer").text();
+  let result = eval(exp);
+
+  $("#answer").text(result);
+});
+
+
+$(".input_ac").on("click",function(){
+  let inputText = $("#answer").text(); 
+  if (inputText !== 0){
+  $("#answer").empty();
+  }
+
+  $("#answer").text(sbNum);
+});
